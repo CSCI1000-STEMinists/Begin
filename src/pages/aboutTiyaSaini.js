5c47a141ce0d58232bd1c2615a5b55509419ecec
Tiya Saini <117602042+tiyasaini3@users.noreply.github.com>
2022-11-18 12:28:27 -0700

    Create aboutTiyaSaini.js

---
+++ src/pages/aboutTiyaSaini.js
@@ -0,0 +1,121 @@
+export default function tiyaPage() {
+    return (
+      <div className='container'>
+        <title>Website</title>
+        <link rel='icon' href='/favicon.ico' />
+
+        <main>
+          <div className='underline'>
+            <h1 className='title'>Hello, I'm Tiya Saini!</h1>
+          </div>
+          <img
+            className='pic'
+            src='http://thoughtcatalog.com/wp-content/uploads/2014/04/shutterstock_144112840.jpg'
+            alt='tiya saini'
+          />
+
+          <p className='description'>
+          My name is Tiya Saini once more, and welcome to my website! I'm going to share some information about me. 
+          I was born in Toronto, Canada, and lived there up until the age of ten before relocating to Denver, Colorado. 
+          Traveling has always been one of my passions, and I hope to do more in the future. 
+          And last, I'm interested in the field of computer science, with data science in sports or artificial intelligence in large technological businesses as my career paths.
+          </p>
+
+          <a className='button' href='/'>
+            <p> Back to Home Page</p>
+          </a>
+        </main>
+
+        <style jsx>{`
+          .container {
+            min-height: 100vh;
+            padding: 0 0.5rem;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            max-width: 60rem;
+            margin: auto;
+          }
+
+          main {
+            padding: 5rem 0;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+
+          .title {
+            margin: 0;
+            line-height: 1.15;
+            font-size: 4rem;
+          }
+
+          .underline {
+            /* border-bottom: solid black; */
+            margin-bottom: 1.5rem;
+          }
+
+          .description {
+            line-height: 1.5;
+            font-size: 1.5rem;
+          }
+
+          .pic {
+            height: 450px;
+            width: 450px;
+          }
+
+          .button {
+            /* margin: .5rem; */
+            flex-basis: 45%;
+            padding: 0.5rem;
+            text-align: left;
+            color: inherit;
+            text-decoration: none;
+            border: 1px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+          }
+
+          .button:hover,
+          .button:focus,
+          .button:active {
+            color: #0070f3;
+            border-color: #0070f3;
+          }
+
+          .button p {
+            font-size: 1rem;
+            font-weight: 600;
+          }
+
+          code {
+            background: #fafafa;
+            border-radius: 5px;
+            padding: 0.75rem;
+            font-size: 1.1rem;
+            font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+              DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+          }
+        `}</style>
+
+        <style jsx global>{`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+          }
+
+          * {
+            box-sizing: border-box;
+          }
+        `}</style>
+      </div>
+    );
+}
